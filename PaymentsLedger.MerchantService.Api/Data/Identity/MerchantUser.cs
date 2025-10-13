614c367f7766341f4d8cf110dd19b04360df9430
Simon Sefcik <sim@Simons-Mac-mini.local>
2025-10-13 15:50:01 +0200

    ef core identity setup done inside merchantes microservice

---
+++ PaymentsLedger.MerchantService.Api/Data/Identity/MerchantUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace PaymentsLedger.MerchantService.Api.Data.Identity;
+
+public class MerchantUser : IdentityUser<Guid>
+{
+}
--- PaymentsLedger.MerchantService.Api/Program.cs
+++ PaymentsLedger.MerchantService.Api/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PaymentsLedger.MerchantService.Api.Data;
+using PaymentsLedger.MerchantService.Api.Data.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,58 @@ builder.AddNpgsqlDbContext<MerchantDbContext>(connectionName: "merchantsdb");
 // Register Azure Service Bus client via Aspire integration (connection from AppHost "messaging")
 builder.AddAzureServiceBusClient("messaging");
 
+builder.Services.AddIdentityCore<MerchantUser>(options =>
+{
+    // User
+    options.User.RequireUniqueEmail = true;
+
+    // Password policy
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 12;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+
+    // Lockout policy
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+})
+.AddRoles<MerchantRole>()
+.AddEntityFrameworkStores<MerchantDbContext>()
+.AddSignInManager();
+
+// Required by SignInManager and Identity validators
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<MerchantDbContext>();
+    await dbContext.Database.MigrateAsync();
+
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<MerchantRole>>();
+
+    async Task EnsureRoleAsync(string name)
+    {
+        if (!await roleManager.RoleExistsAsync(name))
+        {
+            var role = new MerchantRole
+            {
+                Name = name,
+                NormalizedName = name.ToUpperInvariant()
+            };
+
+            await roleManager.CreateAsync(role);
+        }
+    }
+
+    await EnsureRoleAsync(MerchantRole.Standard);
+    await EnsureRoleAsync(MerchantRole.Premium);
+}
 
 app.MapGet("/", () => "Merchant Service up");
 
-app.Run();
+await app.RunAsync();
