68f077c6448bd2171eeb7dd875049d55ba0d9906
Simon Sefcik <sefcik.simon@gmail.com>
2025-10-23 14:24:24 +0200

    basic implementation of saving payment object to db implemented

---
+++ PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Application/Aggregates/PaymentAggregate/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+using PaymentsLedger.PaymentService.Domain.PaymentAggregate;
+
+namespace PaymentsLedger.PaymentService.Application.Aggregates.PaymentAggregate;
+
+public interface IPaymentRepository
+{
+    Task AddAsync(Payment payment, CancellationToken cancellationToken = default);
+}
+
--- PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Infrastructure/DataAccessRegistration.cs
+++ PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Infrastructure/DataAccessRegistration.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PaymentsLedger.PaymentService.Infrastructure.Persistence;
 using PaymentsLedger.PaymentService.Application.Aggregates.PaymentAggregate.Commands.PaymentCreated;
+using PaymentsLedger.PaymentService.Application.Aggregates.PaymentAggregate;
+using PaymentsLedger.PaymentService.Infrastructure.Repositories;
 
 namespace PaymentsLedger.PaymentService.Infrastructure;
 
@@ -13,6 +15,9 @@ public static class DataAccessRegistration
         builder.AddNpgsqlDbContext<PaymentDbContext>(connectionName: "paymentsdb");
 
         builder.Services.AddInProcMessaging();
+
+        // Repositories
+        builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 
         // Apply EF Core migrations for PaymentDbContext at startup
         builder.Services.AddHostedService<PaymentDbMigratorHostedService>();
