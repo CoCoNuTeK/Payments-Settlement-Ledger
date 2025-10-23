91ba9ba73d37b7b30197aeb3c076f6c54523c82f
Simon Sefcik <sefcik.simon@gmail.com>
2025-10-23 16:10:09 +0200

    gave better naming to our payment created command

--- PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Application/Aggregates/PaymentAggregate/Commands/PaymentCreated/IPaymentCreatedCommandHandler.cs
+++ PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Application/Aggregates/PaymentAggregate/Commands/PaymentCreated/IPaymentCreatedCommandHandler.cs
@@ -2,8 +2,7 @@ using PaymentsLedger.PaymentService.Domain.PaymentAggregate;
 
 namespace PaymentsLedger.PaymentService.Application.Aggregates.PaymentAggregate.Commands.PaymentCreated;
 
-public interface IPaymentCreatedHandler
+public interface IPaymentCreatedCommandHandler
 {
     Task HandleAsync(Payment payment, CancellationToken cancellationToken = default);
 }
-
