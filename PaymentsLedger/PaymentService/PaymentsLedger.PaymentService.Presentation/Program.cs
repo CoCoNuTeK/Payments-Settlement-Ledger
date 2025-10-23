ad8684a02670c9e472d685a4abd7436762fc1bf2
Simon Sefcik <sefcik.simon@gmail.com>
2025-10-23 10:53:30 +0200

    fix placement of db migrations update from presentation to infra layer

--- PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Presentation/Program.cs
+++ PaymentsLedger/PaymentService/PaymentsLedger.PaymentService.Presentation/Program.cs
@@ -1,19 +1,10 @@
-using Microsoft.EntityFrameworkCore;
 using PaymentsLedger.PaymentService.Infrastructure;
-using PaymentsLedger.PaymentService.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Register infrastructure services
 builder.AddInfra();
 var app = builder.Build();
-
-// Ensure database is migrated; seeding is handled via UseSeeding/UseAsyncSeeding
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<PaymentDbContext>();
-    await db.Database.MigrateAsync();
-}
 
 app.MapGet("/", () => "Hello World!");
 
