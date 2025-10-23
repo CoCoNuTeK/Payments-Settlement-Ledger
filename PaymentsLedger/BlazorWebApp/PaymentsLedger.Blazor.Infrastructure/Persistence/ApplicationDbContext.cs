86b52314fc27ab56515b1c8e120fcae2b5d2d475
Simon Sefcik <sim@Simons-Mac-mini.local>
2025-10-23 18:35:57 +0200

    revert seeding to older approach given this is efcore with identity such that seeding is correctly done in ihosted service

--- PaymentsLedger/BlazorWebApp/PaymentsLedger.Blazor.Infrastructure/Persistence/ApplicationDbContext.cs
+++ PaymentsLedger/BlazorWebApp/PaymentsLedger.Blazor.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -3,17 +3,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PaymentsLedger.Blazor.Infrastructure.Identity;
-using PaymentsLedger.Blazor.Infrastructure.Seeding;
 
 namespace PaymentsLedger.Blazor.Infrastructure.Persistence;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<MerchantUser, IdentityRole<Guid>, Guid>(options)
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder
-            .UseSeeding((context, isDesignTime) => ApplicationDbContextSeeder.Seed((ApplicationDbContext)context, isDesignTime))
-            .UseAsyncSeeding((context, isDesignTime, ct) => ApplicationDbContextSeeder.SeedAsync((ApplicationDbContext)context, isDesignTime, ct));
-    }
 }
